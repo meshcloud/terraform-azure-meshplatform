481d81783d7be5d11c78b6ecab47091e17200e1b
Mohammad Alhussan <malhussan@meshcloud.io>
2022-04-27 10:21:18 +0200

    refactor: clearer scope variable description

--- modules/meshcloud-replicator-service-principal/variables.tf
+++ modules/meshcloud-replicator-service-principal/variables.tf
@@ -5,7 +5,7 @@ variable "service_principal_name_suffix" {
 
 variable "scope" {
   type        = string
-  description = "The scope to which Service Principal permissions should be assigned to. Usually this is a management group that sits atop the subscriptions."
+  description = "The scope to which Service Principal permissions should be assigned to. Usually this is the management group id of form `/providers/Microsoft.Management/managementGroups/<tenantId>` that sits atop the subscriptions."
 }
 
 variable "additional_required_resource_accesses" {
