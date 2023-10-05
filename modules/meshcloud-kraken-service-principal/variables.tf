54146e6e31b65fff5c5d03025032daeb22e35dc5
MJ <mkazemi@meshcloud.io>
2023-10-05 17:43:53 +0200

    feat: rename metering module

--- modules/meshcloud-kraken-service-principal/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_principal_name_suffix" {
-  type        = string
-  description = "Service principal name suffix."
-}
-
-variable "scope" {
-  type        = string
-  description = "The scope to which Service Principal permissions should be assigned to. Usually this is the management group id of form `/providers/Microsoft.Management/managementGroups/<tenantId>` that sits atop the subscriptions."
-}
