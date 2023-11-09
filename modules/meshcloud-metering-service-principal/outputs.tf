ec42672a13d03381422e6cb062c241f7e70bbe28
MJ <mkazemi@meshcloud.io>
2023-11-09 14:28:18 +0100

    fix:metering and replicator output change

--- modules/meshcloud-metering-service-principal/outputs.tf
+++ modules/meshcloud-metering-service-principal/outputs.tf
@@ -3,7 +3,7 @@ output "credentials" {
   value = {
     Enterprise_Application_Object_ID = azuread_service_principal.meshcloud_metering.id
     Application_Client_ID            = azuread_application.meshcloud_metering.application_id
-    Client_Secret                    = "Execute `terraform output metering_credentials` to see the password"
+    Client_Secret                    = "Execute `terraform output metering_client_secret` to see the password"
   }
 }
 
