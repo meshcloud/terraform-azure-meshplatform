ded21399350a7448cacbe1c32bac0ccb00921295
MJ <mkazemi@meshcloud.io>
2023-11-23 16:31:30 +0100

    feat: update azurem and azuread version

--- modules/uami-blueprint-user-principal/outputs.tf
+++ modules/uami-blueprint-user-principal/outputs.tf
@@ -1,7 +1,7 @@
 output "service_principal" {
   value = {
     object_id = azuread_service_principal.uami_blueprint_principal.id
-    app_id    = azuread_service_principal.uami_blueprint_principal.application_id
+    app_id    = azuread_service_principal.uami_blueprint_principal.client_id
     password  = "Execute `terraform output uami_blueprint_user_principal_password` to see the password"
   }
 }
