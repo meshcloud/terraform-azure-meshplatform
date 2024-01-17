0e2476d54e3ec6f31e60ebf78f4b1e0202a681e7
Felix Zieger <fzieger@meshcloud.io>
2024-01-17 11:13:21 +0100

    chore: remove references to cleaned up resources

--- modules/meshcloud-replicator-service-principal/outputs.tf
+++ modules/meshcloud-replicator-service-principal/outputs.tf
@@ -12,10 +12,3 @@ output "application_client_secret" {
   value       = azuread_application_password.application_pw.value
   sensitive   = true
 }
-
-# Terraform does not find the blueprint service principal, even though I find it with
-# ` az ad sp list --filter "appId eq 'f71766dc-90d9-4b7d-bd9d-4499c4331c3f'"`
-# output "blueprint_service_principal_object_id" {
-#   description = "Object ID of the BluePrint Service Principal of this AAD."
-#   value       = data.azuread_application.blueprint_service_principal.object_id
-# }
