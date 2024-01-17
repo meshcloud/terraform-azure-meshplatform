1fbe690347833eabe9c4c17901f5f7e73ee1a543
Felix Zieger <fzieger@meshcloud.io>
2024-01-17 10:48:19 +0100

    chore: remove blueprint module

--- outputs.tf
+++ outputs.tf
@@ -10,11 +10,6 @@ output "replicator_service_principal_password" {
   sensitive   = true
 }
 
-# output "blueprint_service_principal_object_id" {
-#   description = "Object ID of the BluePrint Service Principal in this AAD."
-#   value       = length(module.replicator_service_principal) > 0 ? module.replicator_service_principal[0].azuread_application.blueprint_service_principal.object_id : null
-# }
-
 output "metering_service_principal" {
   description = "Metering Service Principal."
   value       = length(module.metering_service_principal) > 0 ? module.metering_service_principal[0].credentials : null
@@ -23,28 +18,6 @@ output "metering_service_principal" {
 output "metering_service_principal_password" {
   description = "Password for Metering Service Principal."
   value       = length(module.metering_service_principal) > 0 ? module.metering_service_principal[0].application_client_secret : null
-  sensitive   = true
-}
-
-output "idp_lookup_service_principal" {
-  description = "IDP Lookup Service Principal."
-  value       = length(module.idp_lookup_service_principal) > 0 ? module.idp_lookup_service_principal[0].service_principal : null
-}
-
-output "idp_lookup_service_principal_password" {
-  description = "Password for IDP Lookup Service Principal."
-  value       = length(module.idp_lookup_service_principal) > 0 ? module.idp_lookup_service_principal[0].service_principal_password : null
-  sensitive   = true
-}
-
-output "uami_blueprint_user_principal" {
-  description = "UAMI Blueprint Assignment Service Principal."
-  value       = length(module.uami_blueprint_user_principal) > 0 ? module.uami_blueprint_user_principal[0].service_principal : null
-}
-
-output "uami_blueprint_user_principal_password" {
-  description = "Password for UAMI Blueprint Assignment Service Principal."
-  value       = length(module.uami_blueprint_user_principal) > 0 ? module.uami_blueprint_user_principal[0].service_principal_password : null
   sensitive   = true
 }
 
