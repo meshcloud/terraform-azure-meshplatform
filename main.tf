0f9bce2040faf7fa9a6d359060eadeec24df8495
Felix Zieger <fzieger@meshcloud.io>
2024-07-08 10:46:40 +0200

    chore: specify minimal versions instead of exact versions

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.81.0"
+      version = ">=3.81.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.46.0"
+      version = ">=2.46.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.13.1"
+      version = ">=1.13.1"
     }
   }
 }
